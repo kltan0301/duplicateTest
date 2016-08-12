3e1f9cb296e1ac599ba949aabbb8d0dcd50d4fba
Kai Lin Tan <kailin.tan.2010@gmail.com>
2016-08-12 08:56:59 +0800

    Add test cases

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-function returnDuplicates(arr){
-  var filterArr = [];
-  arr.sort().forEach(function(element, index){
-    if(index !== arr.lastIndexOf(element) && filterArr.indexOf(element) == -1){
-      filterArr.push(element);
+module.exports.getDuplicates = function(arr){
+  if(Array.isArray(arr)){
+    if(arr.some(isNaN)){
+      throw new Error("Invalid Array");
+    }else{
+      var filterArr = [];
+      arr.sort().forEach(function(element, index){
+        if(index !== arr.lastIndexOf(element) && filterArr.indexOf(element) == -1){
+          filterArr.push(element);
+        }
+      });
+      return filterArr;
     }
-  });
-  return filterArr;
-}
+  }else{
+    throw new Error("Type Error");
+  }
+};
--- spec/dupTestSpec.js
+++ spec/dupTestSpec.js
@@ -1,7 +1,33 @@
-require('../app');
+var app = require('../app');
 
-describe('Checks duplicate test function', function(){
-  it('is valid', function(){
-    expect(2+2).toEqual(5);
+describe('Checks getDuplicate test function', function(){
+  it('returns valid array if duplicates', function(){
+    expect(app.getDuplicates([1,2,3,3,4])).toEqual([3]);
+  });
+  it('returns valid array if no duplicates', function(){
+    expect(app.getDuplicates([1,2,3,4,8])).toEqual([]);
+  });
+  it('returns sorted array if duplicates', function(){
+    expect(app.getDuplicates([8,8,2,4,8,7,7])).toEqual([7,8]);
+  });
+  it('returns zero sized array if empty array', function(){
+    expect(app.getDuplicates([])).toEqual([]);
+  });
+  it('doesn\'t allow non arrays', function(){
+    expect(
+      function(){
+        app.getDuplicates(2);
+      }).toThrow("Type Error");
+  });
+  it('doesn\'t allow non integers in array', function(){
+    expect(function(){
+      app.getDuplicates([1,"#",2]);
+    }).toThrow("Invalid Array");
+    expect(function(){
+      app.getDuplicates([1,"abc",2]);
+    }).toThrow("Invalid Array");
+  });
+  it('return duplicate values once only', function(){
+    expect(app.getDuplicates([1,3,3,3,2,2])).toEqual([2,3]);
   });
 });
